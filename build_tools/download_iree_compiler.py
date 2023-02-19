282341bf0f60c49782fd5d4a372449c1a3632a69
Lun Dong <88651178+lundong@users.noreply.github.com>
2023-02-19 09:00:57 -0800

    Bump IREE release to 20220218.434 (#10)
    Also enable emitc inlineHAL.

--- build_tools/download_iree_compiler.py
+++ build_tools/download_iree_compiler.py
@@ -65,7 +65,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="Download IREE host compiler from snapshot releases")
     parser.add_argument(
-        "--tag_name", action="store", default="candidate-20230124.407",
+        "--tag_name", action="store", default="candidate-20230218.434",
         help="snapshot tag to download. If not set, default to the one synced to third_party/iree commit.")
     parser.add_argument(
         "--release_url", action="store",
